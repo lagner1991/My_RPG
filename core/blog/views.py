3e00d61ad42725542e27272e5577ac2c29af7ad5
HP <lagner@gmail.com>
2022-05-07 19:05:50 +0300

    Created and connect templates root directory
    html application files have been moved to the root templates folder

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    context_dict = {'my_name': 'LAGNER_1991', 'names': ['Eva', 'Olga', 'Tata']}
+    context_dict = {'one': 'первый', 'two': 'второй', 'three': 'третий'}
     return render(request, template_name='blog/index.html', context=context_dict)
